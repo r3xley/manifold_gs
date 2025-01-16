f01e4b3e90c7f6944a75c3726dfdf16ac52fa0bc
Robert Exley <78571570+r3xley@users.noreply.github.com>
2025-01-16 16:44:27 -0500

    Add helper.py

--- main_sfm.py
+++ main_sfm.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import open3d
+from helper import * 
 
 
-print("hello")
+def main():
+    path1 = "Data/0000.jpg"
+    image = cv2.imread(path1, )
+
+
+    plt.imshow(image)
+    plt.show()
