e5489591dbe0e8899d392a96c92f5c9c84863144
Robert Exley <78571570+r3xley@users.noreply.github.com>
2025-01-19 22:27:52 -0500

    fundamental matrix method

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def extract_keypoints(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -12,3 +13,16 @@ def match_keypoints(des1, des2):
     matches = sorted(matches, key = lambda x:x.distance)
 
     return matches
+
+def fund_matrix(kp1, kp2, matches):
+    pts1 = []
+    pts2 = []
+    for match in matches:
+        pts1.append(kp1[match.queryIdx].pt)
+        pts2.append(kp2[match.trainIdx].pt)
+    
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+
+    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+    return F, mask, pts1, pts2
