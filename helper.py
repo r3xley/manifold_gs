0aaab357899a3d1977d96cea34034255f174eedb
Robert Exley <78571570+r3xley@users.noreply.github.com>
2025-01-21 16:34:28 -0500

    recover pose frm essential matrix

--- helper.py
+++ helper.py
@@ -40,3 +40,22 @@ def essential_matrix(kp1, kp2, matches, K):
     E, mask = cv2.findEssentialMat(pts1, pts2, K, method=cv2.RANSAC, prob=0.999, threshold=1.0)
 
     return E, mask, pts1, pts2
+
+def recover_pose(E, K, pts1, pts2):
+    _, R, t, mask = cv2.recoverPose(E, pts1, pts2, K)
+    return R, t
+
+def draw_epipolar_lines(img1, img2, lines, pts1, pts2):
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    r, c = img1.shape[:2]
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+        color = tuple(np.random.randint(0, 255, 3).tolist())
+        x0, y0 = map(int, [0, -r[2] / r[1]])
+        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
+        img1 = cv2.line(img1, (x0, y0), (x1, y1), color, 1)
+        img1 = cv2.circle(img1, tuple(pt1), 5, color, -1)
+        img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
+    return img1, img2
--- main_sfm.py
+++ main_sfm.py
@@ -30,10 +30,40 @@ def main():
     # Draw matches
     img_matches = cv2.drawMatches(image1, kp1, image2, kp2, matches[:20], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     img_matches_rgb = cv2.cvtColor(img_matches, cv2.COLOR_BGR2RGB)
-    plt.imshow(img_matches_rgb)
+    #plt.imshow(img_matches_rgb)
+    #plt.show()
+
+    with open("./Data/intrinsic_matrix.txt", 'r') as file:
+        lines = file.readlines()
+    
+    # Extract the matrix values from the custom format
+    matrix_str = ''.join(lines).replace('K = [', '').replace(']', '').replace(';', '').strip()
+    #print(matrix_str)
+    matrix_values = [float(num) for num in matrix_str.split()]
+    #print(matrix_values)
+    matrix = np.array(matrix_values).reshape(3, 3)
+    #print(matrix)
+
+    E, mask, pts1, pts2 = essential_matrix(kp1, kp2, matches, matrix)
+    #print(E)
+    #print(mask)
+    # Draw epipolar lines
+    lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, E)
+    lines1 = lines1.reshape(-1, 3)
+    img1_epilines, img2_points = draw_epipolar_lines(image1, image2, lines1, pts1, pts2)
+
+    lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, E)
+    lines2 = lines2.reshape(-1, 3)
+    img2_epilines, img1_points = draw_epipolar_lines(image2, image1, lines2, pts2, pts1)
+
+    plt.figure(figsize=(15, 5))
+    plt.subplot(121), plt.imshow(img1_epilines)
+    plt.subplot(122), plt.imshow(img2_epilines)
     plt.show()
 
-    
+    #plt.imshow(mask)
+    #plt.show()
+
 
     #img2 = cv2.drawKeypoints(image, kp, None, flags=0)
 
